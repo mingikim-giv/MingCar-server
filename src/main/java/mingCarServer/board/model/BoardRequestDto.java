0e4009bcd918f26f220df36309b67e14c6df56f6
mingikim-giv <mingikim.giv@gmail.com>
2024-05-08 15:56:20 +0900

    Modify: BoardRequestDto get&set

--- src/main/java/mingCarServer/board/model/BoardRequestDto.java
+++ src/main/java/mingCarServer/board/model/BoardRequestDto.java
@@ -1,5 +1,92 @@
 package mingCarServer.board.model;
 
-public class BoardRequestDto {
+import java.sql.Date;
 
+public class BoardRequestDto {
+	private int boardCode;
+	private String id;
+	private String title;
+	private String content;
+	private String author;
+	private boolean category;
+	private Date regWrite;
+	private Date modWrite;
+	
+	public BoardRequestDto(int boardCode, String id, String title, String content, String author, boolean category,
+			Date regWrite, Date modWrite) {
+		super();
+		this.boardCode = boardCode;
+		this.id = id;
+		this.title = title;
+		this.content = content;
+		this.author = author;
+		this.category = category;
+		this.regWrite = regWrite;
+		this.modWrite = modWrite;
+	}
+
+	public int getBoardCode() {
+		return boardCode;
+	}
+
+	public void setBoardCode(int boardCode) {
+		this.boardCode = boardCode;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public boolean isCategory() {
+		return category;
+	}
+
+	public void setCategory(boolean category) {
+		this.category = category;
+	}
+
+	public Date getRegWrite() {
+		return regWrite;
+	}
+
+	public void setRegWrite(Date regWrite) {
+		this.regWrite = regWrite;
+	}
+
+	public Date getModWrite() {
+		return modWrite;
+	}
+
+	public void setModWrite(Date modWrite) {
+		this.modWrite = modWrite;
+	}
+	
 }
