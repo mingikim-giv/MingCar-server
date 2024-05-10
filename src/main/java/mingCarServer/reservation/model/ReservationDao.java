ee1eb150a893f4750fc4cc8d09e3c843a88c15b2
mingikim-giv <mingikim.giv@gmail.com>
2024-05-10 17:40:59 +0900

    Add: instance

--- src/main/java/mingCarServer/reservation/model/ReservationDao.java
+++ src/main/java/mingCarServer/reservation/model/ReservationDao.java
@@ -1,5 +1,19 @@
 package mingCarServer.reservation.model;
 
-public class ReservationDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class ReservationDao {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	private ReservationDao() {}
+	
+	private static ReservationDao instance = new ReservationDao();
+
+	public static ReservationDao getInstance() {
+		return instance;
+	}
 }
