c3ef109ef0fb597c70d059ba4ae8b39c8530ea53
mingikim-giv <mingikim.giv@gmail.com>
2024-05-10 11:46:34 +0900

    Add: CarResponseDto Class

---
+++ src/main/java/mingCarServer/car/model/CarResponseDto.java
@@ -0,0 +1,71 @@
+package mingCarServer.car.model;
+
+public class CarResponseDto {
+	private int carCode;
+	private String carName;
+	private int carPrice;
+	private String carType;
+	private int carSeat;
+	private boolean reservation;
+	
+	public CarResponseDto() {}
+	
+	public CarResponseDto(int carCode, String carName, int carPrice, String carType, int carSeat, boolean reservation) {
+		super();
+		this.carCode = carCode;
+		this.carName = carName;
+		this.carPrice = carPrice;
+		this.carType = carType;
+		this.carSeat = carSeat;
+		this.reservation = reservation;
+	}
+
+	public int getCarCode() {
+		return carCode;
+	}
+
+	public void setCarCode(int carCode) {
+		this.carCode = carCode;
+	}
+
+	public String getCarName() {
+		return carName;
+	}
+
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
+
+	public int getCarPrice() {
+		return carPrice;
+	}
+
+	public void setCarPrice(int carPrice) {
+		this.carPrice = carPrice;
+	}
+
+	public String getCarType() {
+		return carType;
+	}
+
+	public void setCarType(String carType) {
+		this.carType = carType;
+	}
+
+	public int getCarSeat() {
+		return carSeat;
+	}
+
+	public void setCarSeat(int carSeat) {
+		this.carSeat = carSeat;
+	}
+
+	public boolean isReservation() {
+		return reservation;
+	}
+
+	public void setReservation(boolean reservation) {
+		this.reservation = reservation;
+	}
+	
+}
