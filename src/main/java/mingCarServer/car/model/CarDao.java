a8eba663102a31e6cecaca09a6229dd11fd1b266
mingikim-giv <mingikim.giv@gmail.com>
2024-05-10 12:03:12 +0900

    Modify: CarDao(findCarAll)

---
+++ src/main/java/mingCarServer/car/model/CarDao.java
@@ -0,0 +1,53 @@
+package mingCarServer.car.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import mingCarServer.util.DBManager;
+
+public class CarDao {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public CarDao() {}
+	
+	private static CarDao instance = new CarDao();
+	
+	public static CarDao getInstance() {
+		return instance;
+	}
+	
+	public List<CarResponseDto> findCarAll() {
+		List<CarResponseDto> list = new ArrayList<CarResponseDto>();
+		
+		try {
+			conn = DBManager.getConnection();
+			
+			String sql = "SELECT * FROM cars";
+			pstmt = conn.prepareStatement(sql);
+
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				int carCode = rs.getInt(1);
+				String carName = rs.getString(2);
+				int carPrice = rs.getInt(3);
+				String carType = rs.getString(4);
+				int carSeat = rs.getInt(5);
+				boolean reservation = rs.getBoolean(6);
+				
+				CarResponseDto temp = new CarResponseDto(carCode, carName, carPrice, carType, carSeat, reservation);	
+				list.add(temp);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt);
+		}
+		return list;
+	}
+}
