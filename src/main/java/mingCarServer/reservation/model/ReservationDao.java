98a9ddb01f05e3634a5f4b7f6a701e477b05d2a4
mingikim-giv <mingikim.giv@gmail.com>
2024-05-11 20:23:34 +0900

    Modify: ReservationDao

--- src/main/java/mingCarServer/reservation/model/ReservationDao.java
+++ src/main/java/mingCarServer/reservation/model/ReservationDao.java
@@ -3,6 +3,10 @@ package mingCarServer.reservation.model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import mingCarServer.util.DBManager;
 
 public class ReservationDao {
 	private Connection conn;
@@ -15,5 +19,19 @@ public class ReservationDao {
 
 	public static ReservationDao getInstance() {
 		return instance;
+	}
+	
+	public List<ReservationResponseDto> createReservation(ReservationRequestDto reDto) {
+		List<ReservationResponseDto> list = new ArrayList<ReservationResponseDto>();
+		
+		try {
+			conn = DBManager.getConnection();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list;
 	}
 }
