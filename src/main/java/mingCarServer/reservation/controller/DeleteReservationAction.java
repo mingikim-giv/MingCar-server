a59739bd8f247a332741b8fc5ee69300d7794ae4
mingikim-giv <mingikim.giv@gmail.com>
2024-05-15 22:03:39 +0900

    Add: delete

---
+++ src/main/java/mingCarServer/reservation/controller/DeleteReservationAction.java
@@ -0,0 +1,47 @@
+package mingCarServer.reservation.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mingCarServer.reservation.model.ReservationDao;
+
+/**
+ * Servlet implementation class DeleteReservationAction
+ */
+public class DeleteReservationAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteReservationAction() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		int reCode = Integer.parseInt(request.getParameter("reCode"));
+		ReservationDao resevDao = ReservationDao.getInstance();
+		resevDao.deleteReservation(reCode);
+		
+		response.sendRedirect("/mypage");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
